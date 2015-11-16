1b3b1592ff7b5f0c520fd62a4b2fe5d881c816ff
Mark Chavez <markchav3z@gmail.com>
2015-11-16 20:54:00 +0800

    add more methods to api

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -6,5 +6,17 @@ module Api
       @contacts = Contact.all
       respond_with @contacts
     end
+
+    def show
+      @contact = Contact.find(params[:id])
+      respond_with @contact
+    end
+
+    def create
+      @contact = Contact.new(contact_params)
+      @contact.save
+
+      respond_with @contact
+    end
   end
 end
